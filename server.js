f3634ef439cd9edbfd780d78b29b4ee0d2aff774
DrMrRoboto <margis.ben@gmail.com>
2016-06-29 10:55:13 -0500

    added dependencies to server.js

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 /**
  * Created by BenMargis on 6/29/16.
  */
+
+//Setup================================================================================================================
+
+var express = require('express');
+var app = express();                            //Create the app with express
+var mongoose = require('mongoose');             //mongoose for mongodb
+var morgan = require('morgan');                 //log requests to the console (express4)
+var bodyParser = require('bodyParser');         //pull information from HTML POST (express4)
+var methodOverride = require('methodOveride');  //simulate DELETE and PUT (express 4)
+
+//Configuration========================================================================================================
+
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     //Connect to mongoDB on modulus.io
+
+app.use(express.static(__dirname + '/public'));                     // set the static files location /public/img will be /img for user
+app.use(morgan('dev'));                                             // log every request to the console
+app.use(bodyParser.urlencoded({'extended' : 'true'}));              // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                         // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));      // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// listen (start app with node server.js)==============================================================================
+
+app.listen(8080);
+console.log("app listening on port 8080");
