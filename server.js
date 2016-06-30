478dc72db9ff46581f3264255b5fd74447891354
DrMrRoboto <margis.ben@gmail.com>
2016-06-30 16:30:29 -0500

    Defined model in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var methodOverride = require('methodOveride');  //simulate DELETE and PUT (expre
 
 //Configuration========================================================================================================
 
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     //Connect to mongoDB on modulus.io
+mongoose.connect('');     //Connect to mongoDB on modulus.io
 
 app.use(express.static(__dirname + '/public'));                     // set the static files location /public/img will be /img for user
 app.use(morgan('dev'));                                             // log every request to the console
@@ -21,6 +21,12 @@ app.use(bodyParser.urlencoded({'extended' : 'true'}));              // parse app
 app.use(bodyParser.json());                                         // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));      // parse application/vnd.api+json as json
 app.use(methodOverride());
+
+//Define model=========================================================================================================
+
+var todo = mongoose.model({
+    text : String
+});
 
 // listen (start app with node server.js)==============================================================================
 
